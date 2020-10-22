f604475c88c1bcf1b7f7aec036dc35bc59c04d2e
Aakash <aakash143.jnv@gmail.com>
2020-10-22 19:10:37 +0530

    ok

---
+++ application/views/templates/footer.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script src="javascript/bootstrap4.js" rel="stylesheet" /></script>
+    <script src="jquery/popper.js" rel="stylesheet" /></script>
+    <script src="jquery/jquery-3.2.1.js" rel="stylesheet" /></script>
+</head>
+
+<body>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 m-b-30">
+                    <div class="footer-title m-t-5 m-b-20 p-b-8">
+                        Services
+                    </div>
+                    <div class="footer-links">
+                        <a href="#">
+						Software Development
+						</a>
+                        <a href="#">
+							Payroll
+						</a>
+                        <a href="#">
+							Recruitment
+						</a>
+                        <a href="#">
+							Sports Events
+						</a>
+                    </div>
+                </div>
+                <div class="col-md-3 m-b-30">
+                    <div class="footer-title m-t-5 m-b-20 p-b-8">
+                        Latest themes
+                    </div>
+                    <div class="footer-links">
+                        <a href="#">
+							Appointment
+						</a>
+                        <a href="#">
+							Health center
+						</a>
+                        <a href="#">
+							Quality
+						</a>
+                        <a href="#">
+							Wallstreet
+						</a>
+                    </div>
+                </div>
+                <div class="col-md-3 m-b-30">
+                    <div class="footer-title m-t-5 m-b-20 p-b-8">
+                        Quick Links
+                    </div>
+                    <div class="footer-links">
+                        <a href="#">
+							Blog
+						</a>
+                        <a href="#">
+							FAQ
+						</a>
+                        <a href="#">
+							Terms & conditions
+						</a>
+                        <a href="#">
+							Privacy policy
+						</a>
+                    </div>
+                </div>
+                <div class="col-md-3 m-b-30">
+                    <div class="footer-title m-t-5 m-b-20 p-b-8">
+                        Support
+                    </div>
+                    <div class="footer-links">
+                        <a href="#">
+							Affiliate
+						</a>
+                        <a href="#">
+							Login
+						</a>
+                        <a href="#">
+							All theme package
+						</a>
+                        <a href="#">
+							Support forum
+						</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="footer-bottom">
+        Copyright ©Kanvan Business Solutions Pvt. Ltd.| All rights reserved.
+    </div>
+</body>
+
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kanvan Sports</title>
+    <meta charset="utf-8">
+    <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script src="javascript/bootstrap4.js" rel="stylesheet"></script>
+    <script src="<?php echo base_url(); ?>js/script.js" rel="stylesheet"></script>
+    <script src="jquery/jquery-3.2.1.js" rel="stylesheet"></script>
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
+        <div class="container">
+            <img src="<?php echo base_url(); ?>images/cropped-kanvan-final-av.png" class="navbar-brand logo" alt="Website Development Company">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
