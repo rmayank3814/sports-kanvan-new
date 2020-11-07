3a813b65d8860e3c50605c9fd699f129be837690
raghav-mayank <mayank>
2020-11-07 21:47:14 +0530

    new correction banner and event_news

--- application/views/admin/event_news.php
+++ application/views/admin/event_news.php
@@ -1,21 +1,39 @@
+<style>
+.title-events {
+    font-size: 20px;
+    font-weight:600;
+}
+</style>
+
 <div style="margin-top:100px;"> 
     <?=form_open('admin/event_news',array('method'=>'post')); ?>
-    <div>
-        <label> Event News </label>
-        <input type="text" name="event" value="<?=$event?>">
+    <div class="container">
+    <div class="col-lg-10">
+        <div class="row ">
+            <div class="col-md-12 mb-3">
+                <label class="label-input title-events"> Event News </label>
+                <input type="text" class="form-control form-control-lg" name="event" value="<?=$event?>">
+            </div>
+        </div>
+         <div class="row">
+            <div class="col-md-12 mb-3">
+                <label class="label-input title-events">Comment</label>
+                <input type="text" class="form-control form-control-lg" name="comment" value="<?=$comment?>">
+            </div>
+        </div>
+      <div class="row">
+            <div class="col-md-12 mb-3">
+                <label class="label-input title-events"> Date </label>
+                <input type="date" class="form-control form-control-lg" name="event_date" value="<?=$date?>">
+            </div>
+     </div>
+    <div class="row">
+        <div class="col-md-12 mb-3">
+            <label class="label-input title-events"> Time </label>
+            <input type="time" class="form-control form-control-lg" name="event_time" value="<?=$time?>">
+        </div>
     </div>
-    <div>
-        <label> Comment </label>
-        <input type="text" name="comment" value="<?=$comment?>">
-    </div>
-    <div>
-        <label> Date </label>
-        <input type="date" name="event_date" value="<?=$date?>">
-    </div>
-    <div>
-        <label> Date </label>
-        <input type="time" name="event_time" value="<?=$time?>">
-    </div>
-        <button type="submit" name="event_button">Save</button>
+        <button type="submit" class="btn btn-primary btn-lg" name="event_button">Save</button>
     <?php echo form_close(); ?>
+</div>
 </div>
