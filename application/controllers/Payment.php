aee7897450450f52b37a87743a24e7498ba7f43a
Aakash <aakash143.jnv@gmail.com>
2020-11-18 15:56:38 +0530

    ok

--- application/controllers/Payment.php
+++ application/controllers/Payment.php
@@ -21,14 +21,4 @@ class Payment extends CI_Controller {
         $this->load->view('templates/footer');
     }
 
-    // function verify_details(){
-    //     $cid = $_GET['id'];
-    //     echo $cid;die;
-    //     $this->load->view('templates/header');
-    //     $query = $this->payment_model->fetch_user_data($id);
-    //     $user_data = $query->result_array();
-    //     $this->load->view('payment/verify_details',$user_data[0]);
-    //     $this->load->view('templates/footer');
-    // }
-
 }
