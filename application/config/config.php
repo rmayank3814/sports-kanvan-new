b2cd42badb9d62a6758cbd6c23c65ea7fce8735e
Aakash <aakash143.jnv@gmail.com>
2020-12-15 11:30:23 +0530

    ok

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:91/Codeigniter/sports-kanvan-new/';
+$config['base_url'] = 'http://localhost:8080/Codeigniter/sports-kanvan-new/';
+
+//$config['base_url'] = 'http://localhost:8080/Codeigniter/sports-kanvan-new/';
+
 /*
 |--------------------------------------------------------------------------
 | Index File
